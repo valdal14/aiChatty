32404d741314956ae6007c09a281853f1c31efa8
Valerio DAlessio <u105550@canda.com>
2025-05-18 10:00:25 +0200

    added the AvatarModel and placed a mock avatar in the `ExploreView`

--- aiChatty/aiChatty/View/Explore/ExploreView.swift
+++ aiChatty/aiChatty/View/Explore/ExploreView.swift
@@ -5,13 +5,21 @@
 //  Created by Valerio D'ALESSIO on 1/5/25.
 //
 
+import UIKit
 import SwiftUI
 
 struct ExploreView: View {
+	let screenWidth: CGFloat = UIScreen.main.bounds.width - 50
+	let avatar: AvatarModel = .mock
 	var body: some View {
 		NavigationStack {
-			Text("Explore")
-				.navigationTitle("Explore")
+			HeroCellView(
+				title: avatar.name,
+				subTitle: avatar.characterDescription,
+				imageURL: avatar.profileImageName,
+				width: screenWidth
+			)
+			.navigationTitle("Explore")
 		}
 	}
 }
